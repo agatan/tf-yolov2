c69cb2e016a7b8a18ab11330180bf6a49cbbbf57
agatan <agatan039@gmail.com>
2018-02-10 15:22:13 +0900

    Extract bounding boxes in keras layer

--- models/yolo.py
+++ models/yolo.py
@@ -12,9 +12,9 @@ M = K.models
 
 
 class YOLO():
-    def __init__(self, input_shape: Tuple[int, int],
+    def __init__(self, image_shape: Tuple[int, int],
                  anchors: np.ndarray, classes: List[str]):
-        self.input_shape = input_shape
+        self.image_shape = image_shape
         self.anchors = anchors
         self.classes = classes
 
@@ -36,7 +36,7 @@ class YOLO():
             n_anchors: number of bouding box anchors
             n_classes: number of classes
         """
-        inputs = L.Input(shape=self.input_shape)
+        inputs = L.Input(shape=self.image_shape)
         darknet_model = M.Model(inputs, darknet19(inputs))
         conv20 = conv2d_bn_leaky(1024, 3)(darknet_model.output)
         conv20 = conv2d_bn_leaky(1024, 3)(conv20)
@@ -50,16 +50,15 @@ class YOLO():
 
         x = L.concatenate([conv21_reshaped, conv20])
         x = conv2d_bn_leaky(1024, 3)(x)
-        x = L.Conv2D(self.n_anchors * (self.n_classes + 5),
-                     1, padding='same')(x)
+        # Tha last FCN layer (= extracted image features for 1/32 scale)
+        image_features = L.Conv2D(self.n_anchors * (self.n_classes + 5),
+                                  1, padding='same')(x)
 
-        self.model = M.Model(inputs, x)
+        outputs = self._extract_bounding_boxes_layer(image_features)
+        self.model = M.Model(inputs, outputs)
         return self.model
 
-    def summary(self):
-        self.model.summary()
-
-    def postprocessing(self, feats: tf.Tensor):
+    def _extract_bounding_boxes_layer(self, image_features):
         """Convert final layer image features to bounding box parameters.
 
         Args:
@@ -80,7 +79,7 @@ class YOLO():
         anchors_tensor = tf.reshape(tf.Variable(self.anchors), [
                                     1, 1, 1, self.n_anchors, 2])
 
-        conv_dims = tf.shape(feats)[1:3]
+        conv_dims = image_features.shape[1:3]
         conv_height_index = K.backend.arange(0, stop=conv_dims[0])
         conv_width_index = K.backend.arange(0, stop=conv_dims[1])
         conv_height_index = tf.tile(conv_height_index, [conv_dims[1]])
@@ -92,22 +91,29 @@ class YOLO():
             tf.stack([conv_height_index, conv_width_index]))
         conv_index = tf.reshape(
             conv_index, [1, conv_dims[0], conv_dims[1], 1, 2])
-        conv_index = tf.cast(conv_index, K.backend.dtype(feats))
+        conv_index = tf.cast(conv_index, image_features.dtype)
 
-        feats = tf.reshape(
-            feats, [-1, conv_dims[0], conv_dims[1], self.n_anchors, self.n_classes + 5])
+        def reshape_lambda(x):
+            return tf.reshape(
+                x, [-1, conv_dims[0], conv_dims[1],
+                    self.n_anchors, self.n_classes + 5])
+        image_features_reshaped = L.Lambda(reshape_lambda)(image_features)
         conv_dims = tf.cast(tf.reshape(
-            conv_dims, [1, 1, 1, 1, 2]), K.backend.dtype(feats))
+            conv_dims, [1, 1, 1, 1, 2]), image_features_reshaped.dtype)
 
-        box_xy = tf.sigmoid(feats[..., :2])
-        box_wh = tf.exp(feats[..., 2:4])
-        box_confidence = tf.sigmoid(feats[..., 4:5])
-        box_class_probs = tf.nn.softmax(feats[..., 5:])
-
-        box_xy = (box_xy + conv_index) / conv_dims
-        box_wh = box_wh * anchors_tensor / conv_dims
+        box_confidence = L.Lambda(lambda x: tf.sigmoid(
+            x[..., 4:5]))(image_features_reshaped)
+        box_class_probs = L.Lambda(lambda x: tf.nn.softmax(
+            x[..., 5:]))(image_features_reshaped)
+        box_xy = L.Lambda(lambda x:
+                          (tf.sigmoid(x[..., :2]) + conv_index) / conv_dims)(image_features_reshaped)
+        box_wh = L.Lambda(lambda x:
+                          tf.exp(x[..., 2:4]) * anchors_tensor / conv_dims)(image_features_reshaped)
 
         return box_xy, box_wh, box_confidence, box_class_probs
+
+    def summary(self):
+        self.model.summary()
 
 
 def print_summary():
@@ -119,7 +125,7 @@ def print_summary():
         [6.63, 11.38],
         [9.42, 5.11],
         [16.62, 10.52]], dtype=np.float32)
-    model = YOLO(input_shape=(224, 224, 3), anchors=sample_anchors,
+    model = YOLO(image_shape=(224, 224, 3), anchors=sample_anchors,
                  classes=[str(i) for i in range(20)])
     model.summary()
 
