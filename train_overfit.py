b8b9826b52918ab066d16f472236bd1cda3e43fc
agatan <agatan039@gmail.com>
2018-02-11 23:02:07 +0900

    Add an example script

---
+++ train_overfit.py
@@ -0,0 +1,49 @@
+"""
+Simple example to clearify how to preprocess dataset, train a YOLO v2 model and do some predictions.
+
+This program is based on https://github.com/allanzelener/YAD2K.
+"""
+
+from models.yolo import YOLO
+
+import numpy as np
+from PIL import Image
+
+def main():
+    # Instantiate YOLO v2 model.
+    model = YOLO(image_shape=(416, 416, 3), classes=['octcat'])
+
+    # Preprocess the target image.
+    orig_image = Image.open('images/sample.png')
+    orig_size = np.array([orig_image.width, orig_image.height])
+
+    image = orig_image.resize((416, 416), Image.BICUBIC)
+    image_data = np.array(image, dtype='float32') / 255.
+
+    # Target boxes.
+    # Each base box is in form of [x_min, y_min, x_max, y_max, class label].
+    base_boxes = np.array([[120, 15, 280, 230, 0]])
+    boxes_xy = 0.5 * (base_boxes[:, 0:2] + base_boxes[:, 2:4]) / orig_size
+    boxes_wh = (base_boxes[:, 2:4] - base_boxes[:, 0:2]) / orig_size
+    # Each box is in form of [y_center, x_center, height, width, class label].
+    boxes = np.concatenate((boxes_xy, boxes_wh, base_boxes[:, -1:]), axis=1)
+    detectors_mask, matching_true_boxes = model.preprocess_true_boxes(boxes)
+
+    # Expand arrays to construct min-batch (batch size is 1 here).
+    image_data_expanded = np.expand_dims(image_data, axis=0)
+    boxes_expanded = np.expand_dims(boxes, axis=0)
+    detectors_mask_expanded = np.expand_dims(detectors_mask, axis=0)
+    matching_true_boxes_expanded = np.expand_dims(matching_true_boxes, axis=0)
+    model.model_loss.fit([
+        image_data_expanded,
+        boxes_expanded,
+        detectors_mask_expanded,
+        matching_true_boxes_expanded,
+                     ], np.zeros(1), batch_size=1, epochs=1)
+
+    model.model_loss.save_weights('overfit_loss_weights.h5')
+    model.model.save_weights('overfit_weights.h5')
+
+
+if __name__ == '__main__':
+    main()
